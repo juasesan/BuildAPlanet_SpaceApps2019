ec7d1cdb4e84c8f04a30c87730eacc53dd2fd707
Enmanuel Magallanes <emmanu2006@hotmail.com>
2019-10-20 14:02:50 -0500

    Pantalla inicio

--- src/buildaplanet/PaneOrganizer.java
+++ src/buildaplanet/PaneOrganizer.java
@@ -2,10 +2,18 @@ package buildaplanet;
 
 import Elementos.SistemaSolar;
 import static buildaplanet.Util.dialogoMaterial;
+import circular.CircularSimplyLinkedList;
 import com.jfoenix.controls.JFXButton;
 import com.jfoenix.controls.JFXSlider;
 import com.jfoenix.controls.JFXTextField;
 import java.io.InputStream;
+import java.io.File;
+import java.net.URI;
+import java.util.Iterator;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javafx.application.Platform;
+>>>>>>> Pantalla inicio
 import javafx.geometry.Insets;
 import javafx.geometry.Orientation;
 import javafx.geometry.Pos;
@@ -25,6 +33,7 @@ import javafx.scene.layout.GridPane;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
 public class PaneOrganizer {
@@ -32,11 +41,13 @@ public class PaneOrganizer {
     private Stage primaryStage;
     private StackPane root;
     private Image imagenFondo;
+    private ImageView img;
+    private boolean end;
     
     
     public PaneOrganizer(Stage primaryStage){
         this.primaryStage = primaryStage;
-       
+        this.end = false;
         System.out.println(getRutaCssFile());
         System.out.println(this.primaryStage.getScene());
         if(this.primaryStage.getScene() != null) {
@@ -53,15 +64,15 @@ public class PaneOrganizer {
      */  
     public StackPane setMenu(){
         StackPane rootMenu = new StackPane();
-        /*
-        imagenFondo = new Image("recursos/PantallaPrincipal/fondoPrincipal.gif",1350,723,true,true);
-        BackgroundImage myBI= new BackgroundImage(imagenFondo,BackgroundRepeat.REPEAT, 
+        
+        imagenFondo = new Image("recursos/PantallaPrincipal/fondoPrincipal.gif",1440,900,true,true);
+        BackgroundImage myBI= new BackgroundImage(imagenFondo,BackgroundRepeat.NO_REPEAT, 
                 BackgroundRepeat.NO_REPEAT, BackgroundPosition.DEFAULT,
           BackgroundSize.DEFAULT);
         //then you set to your node
         rootMenu.setBackground(new Background(myBI));
         //Desarrollo del menu principal
-        */
+        
         VBox center=new VBox();
         GridPane cells=new GridPane();
         cells.setAlignment(Pos.CENTER);
@@ -70,10 +81,14 @@ public class PaneOrganizer {
         center.setAlignment(Pos.CENTER);
         center.setSpacing(68);
         
-        ImageView img = new ImageView(new Image("/recursos/PantallaPrincipal/logoNasa2.png", 480, 203, true, true));
+        img = new ImageView(new Image("/recursos/PantallaPrincipal/ani/1.png", 480, 203, true, true));
         
         center.getChildren().addAll(img,cells);
         
+        
+        Animacion ani = new Animacion();
+        Thread hilo = new Thread(ani);
+        hilo.start();
         
         JFXButton play=new JFXButton("Jugar");
         JFXButton close=new JFXButton("Salir");
@@ -148,6 +163,7 @@ public class PaneOrganizer {
         return systemForm;
     }
     
+
     public ImageView getEntity(String name){
         InputStream rutaStream=getClass().getResourceAsStream("/sprites/"+name+".png");
         Image im=new Image(rutaStream,60,60,true,true);
@@ -155,5 +171,60 @@ public class PaneOrganizer {
         
         return img;
     }
+
+    public class Animacion implements Runnable{
+        
+        
+        CircularSimplyLinkedList<String> c;
+       
+        
+        public Animacion(){
+            
+            c = new CircularSimplyLinkedList<>();
+            boolean b = true;
+            int frame = 1;
+            while(frame <=20 && frame >= 1){
+                String path = "src/recursos/PantallaPrincipal/ani" + "/" + frame + ".png";
+                String p = new File(path).getAbsolutePath();
+                File f = new File(p);
+                URI u = f.toURI();
+                //img.setImage(new Image(u.toString(), 480, 203, true, true));
+                c.addLast(u.toString());
+                System.out.println(frame);
+                if(b) frame++;
+                else frame--;
+                if(frame==21){
+                    frame = 20;
+                    b = false;
+                }
+                
+            }
+            c.removeLast();
+            
+            
+        }
+
+        @Override
+        public void run() {
+            
+           
+            
+            Iterator<String> it = c.iterator();
+            while (!end) {
+
+                try {
+                    img.setImage(new Image(it.next(), 480, 203, true, true));
+                    
+                    
+                    Thread.sleep(20);
+                } catch (InterruptedException ex) {
+                    Logger.getLogger(PaneOrganizer.class.getName()).log(Level.SEVERE, null, ex);
+                }
+            }
+        
+        }
+        
+    }
+
     
 }
